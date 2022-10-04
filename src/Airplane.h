62580a231cab5a190c9931fd44739500b452f054
PJutch <103033969+PJutch@users.noreply.github.com>
2022-10-04 21:12:35 +0300

    Fix licencing

--- src/Airplane.h
+++ src/Airplane.h
@@ -1,4 +1,4 @@
-/* This file is part of JCyberEvolution.
+/* This file is part of Jutchs Shmup.
 
 Jutchs Shmup is free software: you can redistribute it and/or modify it 
 under the terms of the GNU General Public License as published by the Free Software Foundation, 
